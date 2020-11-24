305e2e71a758706c867e98a63b71502a986399ef
JordanRousseau <rousseau.jor@gmail.com>
2020-11-24 14:53:25 +0100

    Oops

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,35 +1,19 @@
 import { IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/react';
 import React from 'react';
 import './ExploreContainer.css';
-import HomeContainer from './HomeContainer';
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
-
-  if(name == "Home"){
-    return ( <div className="container">
-    <IonCard>
-    <IonCardHeader>
-        <IonCardTitle>Import</IonCardTitle>
-      </IonCardHeader>
-
-      <IonCardContent>
-        Go to import
-        </IonCardContent>
-
-    </IonCard>
-</div>)
-  }else{
     return (
       <div className="container">
         <strong>{name}</strong>
         <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     );
-  }
+  
 };
 
 export default ExploreContainer;
